3a961d8f1ce0bc909f1a3a379c25e6fa3cf497fe
unknown <kaiyoung90@mail.fresnostate.edu>
2019-02-25 13:46:21 -0800

    adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'MyFirstAngularApp';
+  contacts = [];
+  constructor() {
+    this.contacts = [
+      {
+        ID: 1,
+        first_name: 'Kai' ,
+        last_name: 'Young',
+        email: 'kaiyoung90@mail.fresnostate.edu'
+      } ,
+      {
+        ID: 2,
+        first_name: 'Meiko' ,
+        last_name: 'Nakahara',
+        email: 'mobara@mail.fresnostate.edu'
+      } ,
+      {
+        ID: 3,
+        first_name: 'John' ,
+        last_name: 'Smith',
+        email: 'jsmith@mail.fresnostate.edu'
+      }
+    ];
+  }
+
+  delete(e) {
+    console.log('from delete e: ', e);
+    this.contacts.splice(e,1);
+  }
+
+  addRow() {
+    this.contacts.unshift(      {
+      ID: 1,
+      first_name: 'Kai' ,
+      last_name: 'Young',
+      email: 'kaiyoung90@mail.fresnostate.edu'
+    });
+  }
+
+  save(row) {
+    console.log('from save row: ', row);
+  }
 }
+
+
